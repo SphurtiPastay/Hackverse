b5d43af40f9f5ac74e3b2022dda50ad5fc779469
alansomathew <alansomathew10@gmail.com>
2023-11-21 13:08:50 +0530

    changes

--- hackverse/urls.py
+++ hackverse/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from hackverse import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
 
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG: urlpatterns += [ re_path(r'^uploads/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}),]
